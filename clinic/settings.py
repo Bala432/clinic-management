f13e5fca454089f02581dc65cf9a9f919e5b88d8
Bala432 <45410042+Bala432@users.noreply.github.com>
2021-11-10 09:02:15 +0530

    Update settings.py

--- clinic/settings.py
+++ clinic/settings.py
@@ -56,8 +56,8 @@ ROOT_URLCONF = 'clinic.urls'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "balaviswanadh21@gmail.com"
-EMAIL_HOST_PASSWORD = "bala@78365"
+EMAIL_HOST_USER = "********"
+EMAIL_HOST_PASSWORD = "********"
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
